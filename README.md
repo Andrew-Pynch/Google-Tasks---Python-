2cd645c62fb1ebdfa41fe3ee3846b1d2dce4ae3c
Andrew Pynch <andrewpynchbusiness@gmail.com>
2020-04-27 20:10:31 -0700

    Finished script to get task items from task list by name

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Google-Tasks---Python-
 Needed to use the Google tasks API for a side project. Wrote a couple scripts I found helfpul. 
+
+# Getting Started
+Follow the setup on the official docs for Google Tasks. 
+[Official Docs](https://developers.google.com/tasks/quickstart/python)
+Once you have your
+```
+credentials.json and token.pickle
+``` 
+files in your local directory run
+```python
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,72 @@
+from __future__ import print_function
+import sys
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+def getTaskListIDByName(taskListName: str, service):
+    all_task_lists = service.tasklists().list().execute()
+
+    if not all_task_lists['items']:
+        print("Nothing found")
+    else:
+        for task_list in all_task_lists['items']:
+            if task_list['title'] == taskListName:
+                return task_list['id']
+
+
+def checkCMDArgs():
+    if len(sys.argv) > 1:
+        return sys.argv[1]
+    else:
+        task_list_name_param = input("Please enter the name of the task list: ")
+        return task_list_name_param
+         
+
+def main():
+    task_list_name_param = checkCMDArgs()
+
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+
+    service = build('tasks', 'v1', credentials=creds) 
+
+    task_list_name = getTaskListIDByName(task_list_name_param, service)
+
+    tasks = service.tasks().list(tasklist=task_list_name).execute()
+
+    for task in tasks['items']:
+        print(task['title'])
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+    
+
+        
+
+# def main(taskListName):
+#     if os.path.exists('token.pickle'):
+#         with open('token.pickle', 'rb') as token:
+#             creds = pickle.load(token)
+
+#     service = build('tasks', 'v1', credentials=creds)
+
+#     all_list = service.tasklists().list().execute()
+
+#     if not all_list['items']:
+#         print("Nothing found")
+#     else:
+#         for task_list in all_list['items']:
+#             if task_list['title'] == taskListName:
+#                 print(task_list['id'])
+
+# if __name__ == "__main__":
+#     main('Interviews')
